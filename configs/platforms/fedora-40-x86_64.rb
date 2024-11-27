f2bd0bacfe008b17610e46815f807f5fa601e622
nmburgan <13688219+nmburgan@users.noreply.github.com>
2024-11-27 10:52:28 -0800

    4.0.0-overlookinfra

--- configs/platforms/fedora-40-x86_64.rb
+++ configs/platforms/fedora-40-x86_64.rb
@@ -1,3 +1,5 @@
 platform 'fedora-40-x86_64' do |plat|
   plat.inherit_from_default
+  packages = %w(git)
+  plat.provision_with("yum install -y #{packages.join(' ')}")
 end
--- configs/platforms/ubuntu-24.04-amd64.rb
+++ configs/platforms/ubuntu-24.04-amd64.rb
@@ -1,4 +1,4 @@
-platform "debian-12-amd64" do |plat|
+platform "ubuntu-24.04-amd64" do |plat|
   plat.inherit_from_default
   packages = %w(git)
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
